0da67ac9bf3f1db639edd6e48661d78325edbca4
agr6014 <agr4106@gmail.com>
2026-07-07 17:03:03 +0100

    Fragment loading and transactions are now capable (later functionality and switching to be added)

--- app/src/main/java/com/example/comp2000_referral_work_10909750/MainActivity.java
+++ app/src/main/java/com/example/comp2000_referral_work_10909750/MainActivity.java
@@ -1,4 +1,32 @@
 package com.example.comp2000_referral_work_10909750;
 
-public class MainActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_activity);
+        // This sets the main layout as main_activity.xml
+        // This is good due to it being the fragment container
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_container, new fragment_start()).commit();
+        }
+        // First checks if the activity has been created before
+        // Then if not it created loads in fragment_start
+    }
+
+    public void openFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, fragment)
+                .addToBackStack(null).commit();
+    }
+    // This allows transistion between the fragments
+
 }
