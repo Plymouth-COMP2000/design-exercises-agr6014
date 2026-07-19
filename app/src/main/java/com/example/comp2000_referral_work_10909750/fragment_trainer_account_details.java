6e9a50002e27824c4d7856ac7815268e83aafb3b
agr6014 <agr4106@gmail.com>
2026-07-19 16:14:07 +0100

    Trainers can now view their own account details. I had to remake the file due to a duplication error, which was extremely annoying

--- app/src/main/java/com/example/comp2000_referral_work_10909750/fragment_trainer_account_details.java
+++ app/src/main/java/com/example/comp2000_referral_work_10909750/fragment_trainer_account_details.java
@@ -14,15 +14,16 @@ import androidx.annotation.NonNull;
 import com.example.comp2000_referral_work_10909750.R;
 // This will let me use "R"
 import com.example.comp2000_referral_work_10909750.MainActivity;
-import  android.widget.TextView;
-
+import android.widget.TextView;
+import com.example.comp2000_referral_work_10909750.api.api;
+import com.example.comp2000_referral_work_10909750.api.user;
+import com.example.comp2000_referral_work_10909750.api.authentication;
 
 public class fragment_trainer_account_details extends Fragment {
-    private database db_helper;
     public fragment_trainer_account_details() {
         super(R.layout.fragment_trainer_account_details);
     }
-    // Makes it so this fragment will be used
+    // Makes this the used fragment
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -39,42 +40,35 @@ public class fragment_trainer_account_details extends Fragment {
 
 
         // Below will show the new added integration
-        db_helper = new database(requireContext());
-        Cursor cursor = db_helper.get_account_id(1);
-        if (cursor.moveToFirst()) {
-            trainer_firstname_text.setText(cursor.getString(cursor.getColumnIndexOrThrow(database.account_firstname)));
-            trainer_lastname_text.setText(cursor.getString(cursor.getColumnIndexOrThrow(database.account_lastname)));
-            trainer_email_text.setText(cursor.getString(cursor.getColumnIndexOrThrow(database.account_email)));
-            trainer_phone_text.setText(cursor.getString(cursor.getColumnIndexOrThrow(database.account_phone)));
+        // db_helper = new database(requireContext());
+        // Cursor cursor = db_helper.get_account_id(1);
+        // if (cursor.moveToFirst()) {
+        // trainer_firstname_text.setText(cursor.getString(cursor.getColumnIndexOrThrow(database.account_firstname)));
+        // trainer_lastname_text.setText(cursor.getString(cursor.getColumnIndexOrThrow(database.account_lastname)));
+        // trainer_email_text.setText(cursor.getString(cursor.getColumnIndexOrThrow(database.account_email)));
+        // trainer_phone_text.setText(cursor.getString(cursor.getColumnIndexOrThrow(database.account_phone)));
 
+        user current_user = authentication.get_current_user_details(requireContext());
+        // This will get the details to then populate the text boxes, showing that i can change them
 
-        }
+        trainer_firstname_text.setText(current_user.getFirstname());
+        trainer_lastname_text.setText(current_user.getLastname());
+        trainer_email_text.setText(current_user.getEmail());
+        trainer_phone_text.setText(current_user.getContact());
 
         trainer_modify_details_button.setOnClickListener(v -> {
             ((MainActivity) requireActivity()).openFragment(
                     new fragment_trainer_modify_details()
             );
 
+
         });
-
-
 
         trainer_back_home.setOnClickListener(v -> {
             ((MainActivity) requireActivity()).openFragment(
                     new fragment_trainer_home()
             );
-
         });
 
-        // The on click listeners will load the fragments wanted (basically changing the UI)
-
-
-    }
-    @Override
-    public void onDestroyView() {
-        if (db_helper != null) {
-            db_helper.close();
-        }
-        super.onDestroyView();
     }
 }
